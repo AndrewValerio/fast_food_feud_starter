d82874047d63c1cfa077871697601edff04796ac
AndrewValerio <andrewvalerio1396@gmail.com>
2022-06-13 16:13:59 -0700

    React First Day

--- src/components/NutritionalLabel/NutritionalLabel.jsx
+++ src/components/NutritionalLabel/NutritionalLabel.jsx
@@ -3,13 +3,21 @@ import { nutritionFacts } from "../../constants"
 import "./NutritionalLabel.css"
 
 export function NutritionalLabel(props) {
+  console.log(props.selectedMenuItem)
   return (
     <div className="nutritional-label">
       <h3 className="title">Nutrition Facts</h3>
 
-      <h4 className="item-name">{`CHANGE_ME`}</h4>
+      <h4 className="item-name">{props.selectedMenuItem.item_name}</h4>
 
-      <ul className="fact-list">{/* WRITE CODE HERE */}</ul>
+      <ul className="fact-list">{nutritionFacts.forEach((fact) => (
+          <NutritionalLabelFact 
+          fact={fact} 
+          key={fact.id} 
+          attribute={fact.attribute}
+          label={fact.label}
+          />
+      ))}</ul>
     </div>
   )
 }
@@ -17,8 +25,8 @@ export function NutritionalLabel(props) {
 export function NutritionalLabelFact(props) {
   return (
     <li className="nutrition-fact">
-      <span className="fact-label">{/* WRITE CODE HERE */}</span>{" "}
-      <span className="fact-value">{/* WRITE CODE HERE */}</span>
+      <span className="fact-label">{props.label}</span>{" "}
+      <span className="fact-value">{props.attribute}</span>
     </li>
   )
 }
